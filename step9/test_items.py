f6b1a61f44e490afd8bf1de6f98859456740fc2f
ZhdemKatyushu <shiroi.maks@gmail.com>
2019-12-24 15:46:02 +0200

    Added files for step9

---
+++ step9/test_items.py
@@ -0,0 +1,14 @@
+﻿from selenium import webdriver
+import time
+import pytest
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_find_the_button(browser):
+    browser.get(link)
+
+    time.sleep(5)
+
+    assert browser.find_element_by_class_name("btn"), "The button is founded!"
+
+    time.sleep(30)
